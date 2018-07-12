617d97516b59848800a50d0bef2c0fd362a9f5f2
Simpolor <simpolor@gmail.com>
2018-07-12 22:52:16 +0900

    게시판 추가 예정

---
+++ src/main/java/com/simpolor/cms/module/board/BoardController.java
@@ -0,0 +1,13 @@
+package com.simpolor.cms.module.board;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+    @RequestMapping("/board/list")
+    public String boardList(){
+        return "Board List";
+    }
+}
