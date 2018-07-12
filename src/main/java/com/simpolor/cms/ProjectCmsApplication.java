7c0eda47d5215d1cf20f6ccc60d51530f494fa55
Simpolor <simpolor@gmail.com>
2018-07-12 22:30:53 +0900

    first commit

---
+++ src/main/java/com/simpolor/cms/ProjectCmsApplication.java
@@ -0,0 +1,12 @@
+package com.simpolor.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCmsApplication.class, args);
+    }
+}
